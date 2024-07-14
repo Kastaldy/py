b5c22d2206772a3232c290c9e13cebbac4505bf4
Felipe Castaldi <101949861+Kastaldy@users.noreply.github.com>
2024-07-14 02:39:15 -0300

    Update Projeto02-Rentabilidade.py

--- Projeto02-Rentabilidade.py
+++ Projeto02-Rentabilidade.py
@@ -10,4 +10,4 @@ rentabilidadeTotal = mes_01 + mes_02 + mes_03 + mes_04 + mes_05
 juros = capital * rentabilidadeTotal 
  # Resultado final
 ganhos = capital + juros
-print('De acordo com o capital inicial e uma rentabilidade de 2% ao mês, esse será seu Patrimônio após 5 meses: ', ganhos)
+print('De acordo com o capital inicial e uma rentabilidade de 2% ao mês, esse será seu capital após 5 meses: ', ganhos)
